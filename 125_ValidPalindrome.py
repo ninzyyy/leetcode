92e401b0750b696c7aed8770d11fa84d605dfce2
Ninaad Kalla <ninaadkalla@gmail.com>
2023-06-21 14:52:53 -0400

    Completed challenge successfully.

---
+++ 125_ValidPalindrome.py
@@ -0,0 +1,18 @@
+def isPalindrome(s:str) -> bool:
+
+    s = s.lower()
+    s = s.replace(" ", "")
+    s_list = [letter for letter in s]
+    s_list = [letter for letter in s_list if letter.isalnum()]
+
+    for n in range(int(len(s_list)/2)):
+        if s_list[n] != s_list[-abs(n+1)]:
+            return False
+    return True
+
+s = 'ra#ce:ca^r'
+answer = isPalindrome(s)
+if answer == True:
+    print('✅ The string is a palindrome.')
+else:
+    print('❌ The string is not a palindrome.')
