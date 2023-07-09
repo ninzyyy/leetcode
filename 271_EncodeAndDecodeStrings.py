cba3b18093c2d06f762eed72ed4e45f20ce43c8c
Ninaad Kalla <ninaadkalla@gmail.com>
2023-07-08 23:59:25 -0400

    Challenge completed succesfully.

--- 271_EncodeAndDecodeStrings.py
+++ 271_EncodeAndDecodeStrings.py
@@ -1,12 +1,14 @@
 
 def encode(strs):
-
     #Adds len(string) + delimiter (!) to the beginning of each string in the list and converts the list to a single string.
+
     encoded_strs = "".join([str(len(string)) + "!" + string for string in strs])
 
     return encoded_strs
 
+
 def decode(string):
+    #Takes the encoded string and decodes it.
 
     decoded_str = []
     i = 0
