cd5f8b1d6480a4a10869752059fa072c7e9b1d58
Ninaad Kalla <ninaadkalla@gmail.com>
2023-07-08 23:27:36 -0400

    Converted encode function to a single line. Added explanation.

--- 271_EncodeAndDecodeStrings.py
+++ 271_EncodeAndDecodeStrings.py
@@ -1,9 +1,9 @@
 
 def encode(strs):
 
-    strs = [str(len(string)) + "!" + string for string in strs]
-    strs = "".join(strs)
+    # Adds len(string) + delimiter (!) to the beginning of each string in the list and converts the list to a single string.
+    encoded_strs = "".join([str(len(string)) + "!" + string for string in strs])
 
-    return strs
+    return encoded_strs
 
 print(encode(['hello', 'world']))
