404cef3145e98541734dd350d15064e98f27dc0b
Ninaad Kalla <ninaadkalla@gmail.com>
2024-01-17 13:47:05 -0500

    69 ms, 17.88 MB

--- 150_EvaluateReversePolishNotation.py
+++ 150_EvaluateReversePolishNotation.py
@@ -5,23 +5,27 @@ def evalRPN(self, tokens: list[str]) -> int:
         stack.append(tokens.pop(0))
 
         if stack[-1] == '+':
-            val = int(stack[-3]) + int(stack[-2])
-            stack = stack[:-3]
-            stack.append(str(val))
+            stack.pop()
+            first = int(stack.pop())
+            second = int(stack.pop())
+            stack.append(second + first)
 
         if stack[-1] == '-':
-            val = int(stack[-3]) - int(stack[-2])
-            stack = stack[:-3]
-            stack.append(str(val))
+            stack.pop()
+            first = int(stack.pop())
+            second = int(stack.pop())
+            stack.append(second - first)
 
         if stack[-1] == '*':
-            val = int(stack[-3]) * int(stack[-2])
-            stack = stack[:-3]
-            stack.append(str(val))
+            stack.pop()
+            first = int(stack.pop())
+            second = int(stack.pop())
+            stack.append(second * first)
 
         if stack[-1] == '/':
-            val = int(float(stack[-3]) / float(stack[-2]))
-            stack = stack[:-3]
-            stack.append(str(val))
+            stack.pop()
+            first = int(stack.pop())
+            second = int(stack.pop())
+            stack.append(int(float(second / first)))
 
     return int(stack[0])
