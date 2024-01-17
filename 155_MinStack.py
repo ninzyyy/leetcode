8af9672f47cdb2df7eb20a087da678229ef6a176
Ninaad Kalla <ninaadkalla@gmail.com>
2024-01-17 11:38:44 -0500

    completed challenge, 41 ms, 21.38 MB, minStack now O(1) instead of O(n) complexity

--- 155_MinStack.py
+++ 155_MinStack.py
@@ -3,18 +3,23 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.minStack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if self.minStack:
+            val = min(val, self.minStack[-1])
+        self.minStack.append(val)
 
     def pop(self) -> None:
         self.stack = self.stack[:-1]
+        self.minStack = self.minStack[:-1]
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.minStack[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
