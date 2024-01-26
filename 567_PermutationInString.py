e2afd51f1a568b57f922d168788f629358723bb9
Ninaad Kalla <ninaadkalla@gmail.com>
2024-01-26 17:56:48 -0500

    added comments

--- 567_PermutationInString.py
+++ 567_PermutationInString.py
@@ -5,14 +5,21 @@ def checkInclusion(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
         return False
 
+    # Initialize pointers and s1 dict
     left = 0
     right = len(s1) - 1
     d1 = Counter(s1)
 
     while right < len(s2):
+
+        # Create window dict
         d2 = Counter(s2[left:right+1])
+
+        # If s1 dict and window match then True
         if d1 == d2:
             return True
+
+        # Shift window to the right
         left += 1
         right += 1
 
