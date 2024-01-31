28f2a79d868f1729f872fed23c29a44e0468cb51
Ninaad Kalla <ninaadkalla@gmail.com>
2024-01-31 12:35:39 -0500

    added iterative solution, 31 ms, 17.50 MB

--- 104_MaximumDepthOfBinaryTree.py
+++ 104_MaximumDepthOfBinaryTree.py
@@ -11,3 +11,17 @@ class Solution:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def maxDepthIterative (self, root: [TreeNode]) -> int:
+
+            stack = [[root, 1]]
+            result = 0
+
+            while stack:
+                node, depth = stack.pop()
+                if node:
+                    result = max(result, depth)
+                    stack.append([node.left, depth + 1])
+                    stack.append([node.right, depth + 1])
+
+            return result
