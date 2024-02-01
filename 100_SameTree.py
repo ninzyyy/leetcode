bed055cff2c7a945d8846c32dd6303669f467b26
Ninaad Kalla <ninaadkalla@gmail.com>
2024-02-01 13:47:42 -0500

    dfs, recursion, 36 ms, 16.58 MB

---
+++ 100_SameTree.py
@@ -0,0 +1,23 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: [TreeNode], q: [TreeNode]) -> bool:
+
+        # Both nodes do not exist
+        if (not p) and (not q):
+            return True
+
+        # One node exists and the other node does not
+        if (not p) or (not q):
+            return False
+
+        # Value of nodes is different
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
