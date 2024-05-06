01dca23478212884925b95d8657939044801b27e
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-06 15:22:13 -0400

    re-attempted problem, cleaned up code and comments

--- 271_EncodeAndDecodeStrings.py
+++ 271_EncodeAndDecodeStrings.py
@@ -1,24 +1,33 @@
-
 def encode(strs):
-    #Adds len(string) + delimiter (!) to the beginning of each string in the list and converts the list to a single string.
-
-    encoded_strs = "".join([str(len(string)) + "!" + string for string in strs])
-
-    return encoded_strs
+    string = ""
+    for s in strs:
+        # Add complex delimiter -> length of string + $
+        # ["i","love","you"] -> "1$i4$love3$you"
+        string += str(len(s)) + "$" + s
+    return string
 
 
 def decode(string):
-    #Takes the encoded string and decodes it.
 
-    decoded_str = []
-    i = 0
+    decoded_str, i = [], 0
 
     while i < len(string):
         j = i
-        while string[j] != '!': #Finds the delimiter.
+        while string[j] != "$":
             j += 1
-        length = int(string[i:j]) #Gets the length of the string after the delimiter.
-        decoded_str.append(string[j+1 : j+1+length]) #Adds the string to the final list.
-        i = j + 1 + length #Sets new "starting" point i
+
+        # Get length of word before delimiter
+        length = int(string[i:j])
+
+        # Get the word
+        word = string[j + 1 : j + 1 + length]
+
+        decoded_str.append(word)
+        i = j + 1 + length
 
     return decoded_str
+
+
+print(decode(encode(["i", "love", "you"])))
+print(decode(encode(["i", "love", "$"])))
+print(decode(encode(["$i", "lo$ve", "$you", "too$"])))
