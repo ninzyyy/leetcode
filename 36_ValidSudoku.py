961fbd645aebe0b44e37510e8a743eab2eb40751
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-06 20:21:01 -0400

    defaultdict(set) and //, 89 ms, 16.62 MB

---
+++ 36_ValidSudoku.py
@@ -0,0 +1,60 @@
+import collections
+
+
+def isValidSudoku(board: list[list[str]]) -> bool:
+
+    # Create dictionaries that initializes key:value pair even if it does not exist
+    cols = collections.defaultdict(set)
+    rows = collections.defaultdict(set)
+    squares = collections.defaultdict(set)
+
+    # Move through coordinates of sudoku board
+    for r in range(9):
+        for c in range(9):
+
+            # Skip if cell is empty
+            if board[r][c] == ".":
+                continue
+
+            # Check if number is already in row, column or square, if so return False
+            if (
+                board[r][c] in rows[r]
+                or board[r][c] in cols[c]
+                or board[r][c] in squares[(r // 3, c // 3)]
+            ):
+                return False
+
+            # Add non-duplicated number to each respective dictionary
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
+
+    return True
+
+
+board1 = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+
+board2 = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+
+print(isValidSudoku(board1))  # True
+print(isValidSudoku(board2))  # False
