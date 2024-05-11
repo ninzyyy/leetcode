ada499e2fe7da71819e3fbe6c4de7d7ba9f2f9d1
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-11 14:35:14 -0400

    pointers, 100 ms, 17.4 MB

--- 167_TwoSumII_InputArrayIsSorted.py
+++ 167_TwoSumII_InputArrayIsSorted.py
@@ -1,17 +1,14 @@
-
 def twoSum(numbers: list[int], target: int):
 
-    L, R = 0, len(numbers) - 1
+    left, right = 0, len(numbers) - 1
+    while left < right:
+        if numbers[left] + numbers[right] > target:
+            right -= 1
+        elif numbers[left] + numbers[right] < target:
+            left += 1
+        else:
+            return [left + 1, right + 1]
 
-    while numbers[R] + numbers[L] != target:
 
-        if numbers[R] + numbers[L] > target:
-            R -= 1
-        elif numbers[R] + numbers[L] < target:
-            L += 1
-
-    return [L+1, R+1]
-
-target = 9
-numbers = [2,7,11,15]
-print(twoSum(numbers, target))
+print(twoSum(numbers=[2, 7, 11, 15], target=9))  # [1, 2]
+print(twoSum(numbers=[2, 3, 4], target=6))  # [1, 3]
