966a00e779636ced1ef22134aa8c7a41f76042d9
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-13 17:11:29 -0400

    pointers, min/max, 511 ms, 29.76 MB

--- 11_ContainerWithMostWater.py
+++ 11_ContainerWithMostWater.py
@@ -1,18 +1,20 @@
-
 def maxArea(height: list[int]) -> int:
 
-    area = 0
     L, R = 0, len(height) - 1
+    maxArea = 0
 
     while L < R:
-        water = min(height[L], height[R]) * (R-L)
-        area = max(area, water)
+        area = (R - L) * min(height[R], height[L])
+        maxArea = max(maxArea, area)
 
         if height[L] < height[R]:
             L += 1
         else:
             R -= 1
-    return area
 
-tank = [1,8,6,2,5,4,8,3,7]
-print(maxArea(tank))
+    return maxArea
+
+
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))  # 49
+print(maxArea([1, 1]))  # 1
+print(maxArea([4, 3, 2, 1, 4]))  # 16
