94f3c2c518f9d4e07a39bd35c9d779cc5923c97b
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-14 16:34:48 -0400

    added test cases

--- 150_EvaluateReversePolishNotation.py
+++ 150_EvaluateReversePolishNotation.py
@@ -1,18 +1,25 @@
-
-def evalRPN(self, tokens: list[str]) -> int:
+def evalRPN(tokens: list[str]) -> int:
 
     stack = []
+
     for t in tokens:
-        if t not in '+-*/':
+
+        if t not in "+-*/":
             stack.append(int(t))
+
         else:
             num = stack.pop()
-            if t == '+':
+            if t == "+":
                 stack[-1] += num
-            elif t == '-':
+            elif t == "-":
                 stack[-1] -= num
-            elif t == '*':
+            elif t == "*":
                 stack[-1] *= num
             else:
                 stack[-1] = int(stack[-1] / num)
+
     return stack[0]
+
+
+print(evalRPN(["2", "1", "+", "3", "*"]))  # 9
+print(evalRPN(["4", "13", "5", "/", "+"]))  # 6
