70b12c9903c014ebcfc71383aee8829c5782bfd5
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-14 14:47:15 -0400

    added test cases

--- 20_ValidParentheses.py
+++ 20_ValidParentheses.py
@@ -1,14 +1,19 @@
+def isValid(s: str) -> bool:
 
-def isValid(self, s: str) -> bool:
+    d = {"[": "]", "{": "}", "(": ")"}
+    stack = []
 
-        d = {'[':']', '{':'}', '(':')'}
-        stack = []
+    for char in s:
+        if char in d:
+            stack.append(char)
 
-        for char in s:
-            if char in d:
-                stack.append(char)
+        elif len(stack) == 0 or d[stack.pop()] != char:
+            return False
 
-            elif len(stack) == 0 or d[stack.pop()] != char:
-                return False
+    return stack == []
 
-        return stack == []
+
+print(isValid("()"))  # True
+print(isValid("()[]{}"))  # True
+print(isValid("(]"))  # False
+print(isValid("([)]"))  # False
