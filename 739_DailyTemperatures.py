d33cdafcf030a1ab3db1abd8d2b40397d2590d55
Ninaad Kalla <ninaadkalla@gmail.com>
2024-05-20 21:13:30 -0400

    monotonic decreasing stack, 876 ms, 31.42 MB

--- 739_DailyTemperatures.py
+++ 739_DailyTemperatures.py
@@ -1,14 +1,15 @@
-
 def dailyTemperatures(temperatures):
-    answer = [0] * len(temperatures) # Create a zeroes array to "fill"
+
+    res = [0] * len(temperatures)  # Create a zeroes array to "fill"
     stack = []
 
-    for index, temp in enumerate(temperatures):
-        while stack and temp > stack[-1][1]:
-            stack_i, stack_t = stack.pop()
-            answer[stack_i] = index - stack_i
-        stack.append([index, temp])
-        print(answer)
-    return answer
+    for i in range(len(temperatures)):
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            j = stack.pop()
+            res[j] = i - j
+        stack.append(i)
+    return res
 
-print(dailyTemperatures([73,74,75,71,69,72,76,73]))
+
+print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))  # [1,1,4,2,1,1,0,0]
+print(dailyTemperatures([20, 40, 60]))  # [1, 1, 0]
